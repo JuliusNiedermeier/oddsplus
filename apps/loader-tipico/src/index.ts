f4f3beffe6d25d939c8ee4b62691155b613e7373
JuiusNiedermeier <juius.niedermeier@outlook.de>
2023-03-08 16:35:45 +0100

    Log number of changes
    Log number of creates, updates and deletes for sports, competitions and events

--- apps/loader-tipico/src/index.ts
+++ apps/loader-tipico/src/index.ts
@@ -1,12 +1,48 @@
 import { getCompetitions, getEvents, getSports } from "./normalize.js";
+import { diffEntityArrays } from "./diffEntityArrays.js";
 import { streamData } from "./streamData.js";
+import { Event, Sport, Competition } from "./types/index.js";
 
-const unsubscribe = streamData((data) =>
-  console.log({
-    sports: getSports(data).length,
-    competitions: getCompetitions(data).length,
-    events: getEvents(data).length,
-  })
-);
+let previousSports: Sport[] = [];
+let previousCompetitions: Competition[] = [];
+let previousEvents: Event[] = [];
 
-setTimeout(unsubscribe, 10000);
+const unsubscribe = streamData((data) => {
+  console.time("Analyzed data in");
+  // Sports
+  const sports = getSports(data);
+  const changedSports = diffEntityArrays(previousSports, getSports(data));
+  previousSports = sports;
+
+  // Competitions
+  const competitions = getCompetitions(data);
+  const changedCompetitions = diffEntityArrays(
+    previousCompetitions,
+    getCompetitions(data)
+  );
+  previousCompetitions = competitions;
+
+  // Events
+  const events = getEvents(data);
+  const changedEvents = diffEntityArrays(previousEvents, getEvents(data));
+  previousEvents = events;
+
+  console.timeEnd("Analyzed data in");
+  console.table({
+    sports: {
+      created: changedSports.created.length,
+      updated: changedSports.updated.length,
+      deleted: changedSports.deleted.length,
+    },
+    competitions: {
+      created: changedCompetitions.created.length,
+      updated: changedCompetitions.updated.length,
+      deleted: changedCompetitions.deleted.length,
+    },
+    events: {
+      created: changedEvents.created.length,
+      updated: changedEvents.updated.length,
+      deleted: changedEvents.deleted.length,
+    },
+  });
+});
